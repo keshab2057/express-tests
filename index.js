6b3af28efdb61895fb6b0d0534237dd60fcf33c7
keshab sutar <keshabsingh981829@gmail.com>
2024-01-30 22:09:16 +0545

    api data sending mechanism setup

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
 const express = require("express");
-const app = express();
+const app = express(); //express intialze-->app ma store
 const PORT = 8000;
-app.get("/",(req,res)=>{
-    res.json({msg:"hello world"});
+app.use(express());
+//READ
+app.get("/", (req, res) => {
+  //logic = database
+  res.json({ msg: "All blog list:" });
 });
-app.listen(PORT,()=>{
-    console.log(`app is running on port ${PORT}.url is http://localhost:${PORT}`);
+
+//CREATE
+app.post("/", (req, res) => {
+  //logic=>send this data to db
+  const data = req.body;
+  res.json({ msg: `New blog added with data:${data}` });
 });
 
-//GET-->fetch data from server to clint
-//POST-->create new data(clint to server
-//PUT-->update more than one data of existing data(clint to server
-//PATCH-->update only one data of existing data (clint to server)
-//DELETE-->remove existing data(server to clint)
+//UPDATE
+app.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  //send params with this data to db
+  res.json({ msg: `Block id ${id} put with data ${data}` });
+});
 
+//UPDATED
+app.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body; //another method ma data string ma aaux so body bata garyo vahne sting data lai string ra number data lai number format ma dinx
+  //send params with this data to db
+  res.json({ msg: `Block id ${id} patch with data ${data}` });
+});
 
+//REMOVE
+app.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  res.json({ msg: `Block id ${id} deleted` });
+});
 
+app.listen(PORT, () => {
+  console.log(`App is running at port ${PORT} url is http://localhost:${PORT}`);
+});
+
+//data sending mechanism (front end to backend)
+/*
+ 1.params-->dynamic values eg:(/:id,/:email)
+ 5.query ? name=keshab and age=40;
+ 2.body--> raw json{}
+ 3.headers
+ 4.query
+ */
