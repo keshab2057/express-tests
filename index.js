5f55c32d0244e02cdbb66e5fbbd0b6f9fc0d7e48
keshab sutar <keshabsingh981829@gmail.com>
2024-02-03 12:23:32 +0545

    url building with get,post,put,patch,delete method

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 const express = require("express");
-const indexRouter = require("./routes");
 const app = express();
-const PORT = 8000;
-app.use(express.json());//req body
+PORT = 8000;
+app.use(express.json());
 
-app.use("/assets",express.static("public"))//static files
-
-app.use((req,res,next)=>{
-req.body.currency = "NPR";
-next();
+//READ
+app.get("/", (req, res) => {
+  res.json({ msg: "All blog list" });
 });
 
-app.use("/",indexRouter);
-
-app.use((err,req,res,next)=>{
-err = err?err.toString() :"something went wrong";
-res.status(500).json({msg:err});
+//CREATE
+app.post("/", (req, res) => {
+  const { data } = req.body;
+  console.log(req.body);
+  //logic=>send this data to database
+  res.json({ msg: `new blog added with data:${data}` });
 });
 
+//UPDATE
+app.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { data } = req.body;
+  console.log(req.body);
+  console.log(req.params);
+  res.json({ msg: `add blog id ${id} put with data ${data}` });
+});
+
+//UPDATE
+app.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const { data } = req.body;
+  res.json({ msg: `add blog id ${id} patch with data ${data}` });
+});
+
+//DELETE
+app.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  res.json({ msg: `blog id ${id} deleted` });
+});
 
 app.listen(PORT, () => {
-  console.log(`App is running at port ${PORT} url is http://localhost:${PORT}`);
+  console.log(`App is running at port ${PORT}.url is http://localhost:${PORT}`);
 });
-
